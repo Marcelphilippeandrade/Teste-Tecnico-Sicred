6735004b2cbe07f8a5315bd4811a64053af0b654
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2026-02-19 14:07:54 -0300

    Criação inicial do projeto com a estrutura inicial do projeto.

---
+++ src/main/java/br/com/sicredi/votacao/SicrediVotacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediVotacaoApplication.class, args);
+	}
+
+}
