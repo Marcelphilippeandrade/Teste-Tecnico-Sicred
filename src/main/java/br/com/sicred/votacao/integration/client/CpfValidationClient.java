7af611cda7f8850509c3045b8cdcb21a62d1dc39
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2026-02-20 19:28:20 -0300

    Refatoração para eleminar codigo hardcode na chamada da API externa de validação de CPF

--- src/main/java/br/com/sicred/votacao/integration/client/CpfValidationClient.java
+++ src/main/java/br/com/sicred/votacao/integration/client/CpfValidationClient.java
@@ -1,6 +1,5 @@
 package br.com.sicred.votacao.integration.client;
 
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -8,30 +7,33 @@ import org.springframework.web.client.RestTemplate;
 import br.com.sicred.votacao.enums.CpfVoteStatus;
 import br.com.sicred.votacao.exception.business.CpfInvalidoException;
 import br.com.sicred.votacao.exception.business.CpfNaoPodeVotarException;
+import br.com.sicred.votacao.integration.config.CpfValidationProperties;
 import br.com.sicred.votacao.integration.dtos.CpfValidationResponse;
 
 @Component
 public class CpfValidationClient {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+	private final RestTemplate restTemplate;
+	private final CpfValidationProperties properties;
 
-    private static final String URL =
-        "https://user-info.herokuapp.com/users/{cpf}";
+	public CpfValidationClient(RestTemplate restTemplate, CpfValidationProperties properties) {
+		this.restTemplate = restTemplate;
+		this.properties = properties;
+	}
 
-    public void validarCpfParaVoto(String cpf) {
+	public void validarCpfParaVoto(String cpf) {
 
-        try {
-            CpfValidationResponse response =
-                restTemplate.getForObject(URL, CpfValidationResponse.class, cpf);
+		String url = properties.getBaseUrl() + "/users/{cpf}";
 
-            if (response == null ||
-                CpfVoteStatus.UNABLE_TO_VOTE.name().equals(response.status())) {
-                throw new CpfNaoPodeVotarException();
-            }
+		try {
+			CpfValidationResponse response = restTemplate.getForObject(url, CpfValidationResponse.class, cpf);
 
-        } catch (HttpClientErrorException.NotFound ex) {
-            throw new CpfInvalidoException();
-        }
-    }
+			if (response == null || CpfVoteStatus.UNABLE_TO_VOTE.name().equals(response.status())) {
+				throw new CpfNaoPodeVotarException();
+			}
+
+		} catch (HttpClientErrorException.NotFound ex) {
+			throw new CpfInvalidoException();
+		}
+	}
 }
-
